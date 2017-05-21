e0b797c8000c1393d7d080f4a8648e93f9e4342a
kamfor <kamfor>
2017-05-21 20:41:03 +0200

    ojojojojojojojojojprawi PID

--- 2/transmitancja.m
+++ 2/transmitancja.m
@@ -1,4 +1,5 @@
 %% inicjalizacja
+clear all; 
 K = 3.5; 
 T0 = 5; 
 T1 = 2;
@@ -6,41 +7,24 @@ T2 = 5.02;
 s1 = 10.04; 
 s2 = 7.02; 
 s3 = 1; 
+t_sym=50;
+y_zad=1;
 
 %% Obliczenie transmitancji dyskretnej
-
 s = tf('s');
-G = K*exp(-T0*s)/(s1*s^2+s2*s*s3);
+G = K*exp(-T0*s)/(s1*s^2+s2*s+s3);
 Gd = c2d(G,0.5,'zoh');
-G2 = 1*exp(-5*s)/(100*s+1);
 %% odpowiedz skokowa
-% y=step(Gd);
-% step(Gd) 
-% hold on;
-figure;
-czas_sym=50;
-y_zad=1;
-% subplot(1,2,2)
-% step(G,czas_sym)
-% hold on;
-% stairs(0:czas_sym,[0 0 0 0 0 y_zad*ones(1,czas_sym-4)],'c:');
-% title('b) Obiekt dwuinercyjny');
-% subplot(1,2,1)
-y=step(G,0:0.5:czas_sym);
-t=0:0.5:czas_sym;
-plot(t,y)
-dy=diff(y')./diff(t);
-k=30; % point number 220
-tang=(t-t(k))*dy(k)+y(k);
-hold on
-plot(t,tang)
-scatter(t(k),y(k))
-hold off
-% hold on;
-% stairs(0:czas_sym,[0 0 0 0 0 y_zad*ones(1,czas_sym-4)],'c:');
-% title('Obiekt jednoinercyjny');
-% ylim([-0.2 1.2])
-% xlim([0 czas_sym])
+h = figure;
+set(h,'units','points','position',[10,10,800,500]); 
+y=step(G,0:0.5:t_sym);
+y1=step(Gd,0:0.5:t_sym);
+t=0:0.5:t_sym;
+plot(t,y);
+hold on;
+stairs(t,y1); 
+title('Porównanie transmitancji ciągłej i dyskretnej');
+saveas(h,'1_1','png');
 %% wspólczynniki wzmocnienia statycznego 
 syms s z
 k_s = limit(((5*exp(-5*s))/(10.08*s^2+6.74*s+1)),s,0)
